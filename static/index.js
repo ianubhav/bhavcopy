ba9a5b154988ba4fe02c051fef4b4ae12e335397
Anubhav Agarwal <agarwal.anubhav@iitrpr.ac.in>
2018-02-01 20:34:57 +0530

    modified css

--- static/index.js
+++ static/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   var top_table = $('#top_stocks').DataTable({
+      "paging": false,
       "searching": false
       });
 
@@ -9,31 +10,32 @@ $(document).ready(function() {
 
    $( ".go-button" ).click(function() {
 
-  value = $('.search-text').val()
+    value = $('.search-text').val()
 
-  if (value.length == 0) {
-    return
-  }
+    search_table.clear().draw()
 
-  $('.search tbody tr').remove();
-
-  $(".search").css("visibility", "visible");
-
-  data = {"word" : value}
-  $.post("/search",data,
-    function(data, status) {
-      for (var i = 0; i < data.length; i++) {
-          search_table.row.add( [
-            data[i].code,
-            data[i].name,
-            data[i].open,
-            data[i].high,
-            data[i].low,
-            data[i].close
-        ] ).draw( false );
-      }
+    if (value.length == 0) {
+      return
     }
-  ,dataType = "json");
+
+
+    $(".search_table").css("visibility", "visible");
+
+    data = {"word" : value}
+    $.post("/search",data,
+      function(data, status) {
+        for (var i = 0; i < data.length; i++) {
+            search_table.row.add( [
+              data[i].code,
+              data[i].name,
+              data[i].open,
+              data[i].high,
+              data[i].low,
+              data[i].close
+          ] ).draw( false );
+        }
+      }
+    ,dataType = "json");
 
 });
 
